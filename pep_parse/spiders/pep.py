f15f7fe9f1f972813ac16aeb501757a19b5ff1c7
n.rurenko <eavtorsky@gmail.com>
2023-01-30 07:36:12 +0300

    feat(./pep_parse): spider && items pipeline config

---
+++ pep_parse/spiders/pep.py
@@ -0,0 +1,30 @@
+import re
+import scrapy
+
+from pep_parse.items import PepParseItem
+
+PEP_NUMBER_REGEXP = r'PEP\s(?P<number>\d+)\W+(?P<name>.+)$'
+
+
+class PepSpider(scrapy.Spider):
+    name = 'pep'
+    allowed_domains = ['peps.python.org']
+    start_urls = ['http://peps.python.org/']
+
+    def parse(self, response):
+        peps = response.css('section#numerical-index td a::attr(href)')
+        for pep_link in peps:
+            yield response.follow(pep_link, callback=self.parse_pep)
+
+    def parse_pep(self, response):
+        title = response.css('h1.page-title::text').get().replace('-', '')
+        number, name = re.search(PEP_NUMBER_REGEXP, title).groups()
+        status = (
+            response.css('dt:contains("Status") + dd').css('abbr::text').get()
+        )
+        data = {
+            'number': number,
+            'name': name,
+            'status': status,
+        }
+        yield PepParseItem(data)
